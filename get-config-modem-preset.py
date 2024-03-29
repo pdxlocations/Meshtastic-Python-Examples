5e821f6434fba12089dce838010789a35d45d995
pdxlocations <benlipsey@gmail.com>
2024-03-28 20:12:36 -0700

    add get-config-modem-preset

---
+++ get-config-modem-preset.py
@@ -0,0 +1,16 @@
+import meshtastic.serial_interface
+from meshtastic import config_pb2
+
+interface = meshtastic.serial_interface.SerialInterface()
+
+ourNode = interface.getNode('^local')
+
+lora_config = ourNode.localConfig.lora
+
+# Get the enum value of modem_preset
+modem_preset_enum = lora_config.modem_preset
+
+# Get the string representation of the enum value
+modem_preset_string = config_pb2._CONFIG_LORACONFIG_MODEMPRESET.values_by_number[modem_preset_enum].name
+
+print(modem_preset_string)
