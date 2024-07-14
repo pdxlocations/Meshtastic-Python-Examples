63006877685945f27a3284cbc0ddfe41f787f639
pdxlocations <benlipsey@gmail.com>
2024-07-13 23:43:30 -0700

    import BROADCAST_NUM

--- send-device-metrics.py
+++ send-device-metrics.py
@@ -1,7 +1,8 @@
 try:
     from meshtastic.protobuf import portnums_pb2, telemetry_pb2
+    from meshtastic import BROADCAST_NUM
 except ImportError:
-    from meshtastic import portnums_pb2, telemetry_pb2
+    from meshtastic import portnums_pb2, telemetry_pb2, BROADCAST_NUM
 
 # For connection over serial
 import meshtastic.serial_interface
@@ -11,8 +12,6 @@ interface = meshtastic.serial_interface.SerialInterface()
 # import meshtastic.tcp_interface
 # interface = meshtastic.tcp_interface.TCPInterface(hostname='192.168.1.42', noProto=False)
 
-BROADCAST_ADDR = 4294967295
-
 telemetry_data = telemetry_pb2.Telemetry()
 telemetry_data.device_metrics.battery_level = 69
 telemetry_data.device_metrics.voltage = 4.1
@@ -21,7 +20,7 @@ telemetry_data.device_metrics.air_util_tx = 1
 
 interface.sendData(
     telemetry_data,
-    destinationId=BROADCAST_ADDR,
+    destinationId=BROADCAST_NUM,
     portNum=portnums_pb2.PortNum.TELEMETRY_APP,
     wantResponse=False,
     onResponse=False,
