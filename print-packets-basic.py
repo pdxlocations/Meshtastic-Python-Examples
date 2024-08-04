3394a28f2a45cd79019889395b1ac5e7b3f35221
pdxlocations <benlipsey@gmail.com>
2024-08-04 16:37:28 -0700

    show output as json

--- print-packets-basic.py
+++ print-packets-basic.py
@@ -1,10 +1,12 @@
 import meshtastic.serial_interface
 from pubsub import pub
+import json
 
 interface = meshtastic.serial_interface.SerialInterface()
 
 def onReceive(packet, interface):
-    print(f"{packet} \n\n") 
+    packetJSON = json.dumps(packet, indent=2, default=str)
+    print(f"{packetJSON} \n\n") 
 
 pub.subscribe(onReceive, 'meshtastic.receive')
 
