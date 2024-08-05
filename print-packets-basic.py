7b2d34106f204cdd459a0eb51066be45b20ebbf8
pdxlocations <benlipsey@gmail.com>
2024-08-04 18:43:50 -0700

    add print-packets-json

--- print-packets-basic.py
+++ print-packets-basic.py
@@ -4,20 +4,8 @@ import json
 
 interface = meshtastic.serial_interface.SerialInterface()
 
-def stripnl(s) -> str:
-    return " ".join(str(s).split())
-
-def clean_packet(packet):
-    if 'decoded' in packet and 'telemetry' in packet['decoded'] and 'raw' in packet['decoded']['telemetry']:
-        packet['decoded']['telemetry']['raw'] = stripnl(packet['decoded']['telemetry']['raw'])
-    if 'raw' in packet:
-        packet['raw'] = stripnl(packet['raw'])
-    return packet
-
 def onReceive(packet, interface):
-    packet = clean_packet(packet)
-    packetJSON = json.dumps(packet, indent=2, default=str)
-    print(f"{packetJSON} \n\n")
+    print(f"{packet} \n\n")
 
 pub.subscribe(onReceive, 'meshtastic.receive')
 
