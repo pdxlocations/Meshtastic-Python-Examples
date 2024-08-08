708c3a04b0d368bdcccbff9bac1ee90cbe7aeeb2
pdxlocations <benlipsey@gmail.com>
2024-08-07 22:50:47 -0700

    check for 'decoded'

--- print-messages.py
+++ print-messages.py
@@ -4,9 +4,10 @@ from pubsub import pub
 interface = meshtastic.serial_interface.SerialInterface()
 
 def onReceive(packet, interface):
-    message_bytes = packet['decoded']['payload']
-    message_string = message_bytes.decode('utf-8')
-    print(message_string)
+    if 'decoded' in packet:
+        message_bytes = packet['decoded']['payload']
+        message_string = message_bytes.decode('utf-8')
+        print(message_string)
 
 pub.subscribe(onReceive, 'meshtastic.receive.text')
 
