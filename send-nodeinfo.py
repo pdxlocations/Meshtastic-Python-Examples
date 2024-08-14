d9c4a741f968e6491656a973f5b7482c32e76370
pdxlocations <benlipsey@gmail.com>
2024-08-14 08:36:30 -0700

    add send nodinfo

---
+++ send-nodeinfo.py
@@ -0,0 +1,27 @@
+
+from meshtastic.protobuf import mesh_pb2, config_pb2
+from meshtastic import BROADCAST_NUM
+import meshtastic.serial_interface
+
+interface = meshtastic.serial_interface.SerialInterface()
+
+user = mesh_pb2.User()
+me = interface.nodesByNum[interface.localNode.nodeNum]['user']
+user.id = me['id']
+user.long_name = me['longName']
+user.short_name = me['shortName']
+user.hw_model = mesh_pb2.HardwareModel.Value(me['hwModel'])
+user.role = config_pb2.Config.DeviceConfig.Role.Value(me['role'])
+
+interface.sendData(
+    user,
+    destinationId=BROADCAST_NUM,
+    portNum=meshtastic.portnums_pb2.NODEINFO_APP,
+    wantAck=False,
+    wantResponse=False
+)
+interface.close()
+
+
+
+
