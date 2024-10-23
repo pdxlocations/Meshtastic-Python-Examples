5fb674ecb8a8dc4e7e60715bad42836fbb95ab02
pdxlocations <benlipsey@gmail.com>
2024-10-23 13:15:48 -0700

    add environment metrics

--- send-environment-metrics.py
+++ send-environment-metrics.py
@@ -12,10 +12,21 @@ interface = meshtastic.serial_interface.SerialInterface()
 telemetry_data = telemetry_pb2.Telemetry()
 telemetry_data.environment_metrics.temperature = 352.222
 telemetry_data.environment_metrics.relative_humidity = 69
-telemetry_data.environment_metrics.barometric_pressure = 0
-telemetry_data.environment_metrics.gas_resistance = 0
-telemetry_data.environment_metrics.voltage = 0
-telemetry_data.environment_metrics.current = 0
+# telemetry_data.environment_metrics.barometric_pressure = 0
+# telemetry_data.environment_metrics.gas_resistance = 0
+# telemetry_data.environment_metrics.voltage = 0
+# telemetry_data.environment_metrics.current = 0
+# telemetry_data.environment_metrics.iaq = 0
+# telemetry_data.environment_metrics.distance = 0
+# telemetry_data.environment_metrics.lux = 0
+# telemetry_data.environment_metrics.white_lux = 0
+# telemetry_data.environment_metrics.ir_lux = 0
+# telemetry_data.environment_metrics.uv_lux = 0
+# telemetry_data.environment_metrics.wind_direction = 0
+# telemetry_data.environment_metrics.wind_speed = 0
+# telemetry_data.environment_metrics.wind_gust = 0
+# telemetry_data.environment_metrics.wind_lull = 0
+# telemetry_data.environment_metrics.weight = 0
 
 interface.sendData(
     telemetry_data,
