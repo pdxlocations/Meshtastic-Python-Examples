6440624d06bfdd65057dab9aaab401d720ef7f8b
pdxlocations <benlipsey@gmail.com>
2024-12-02 16:09:40 -0800

    simpler decode

--- decode-protobuf.py
+++ decode-protobuf.py
@@ -1,8 +1,6 @@
-from meshtastic.protobuf import mesh_pb2, telemetry_pb2
+from meshtastic.protobuf import telemetry_pb2
 
-data = mesh_pb2.Data()
 data= b"\r\232+Dg\032\n\r\341z \301\025\000\200\241B"
-
 telem = telemetry_pb2.Telemetry()
 telem.ParseFromString(data)
 
