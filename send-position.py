8f5b63817eff2eb6d8a82fabaa3dca5bdea90726
pdxlocations <benlipsey@gmail.com>
2024-12-07 19:46:32 -0800

    remove time from positon

--- send-position.py
+++ send-position.py
@@ -1,16 +1,12 @@
 import meshtastic.serial_interface
 from meshtastic import BROADCAST_NUM
-import time
 
 interface = meshtastic.serial_interface.SerialInterface()
-
-current_time = int(time.time())
 
 interface.sendPosition(
     latitude = 45,
     longitude = -120,
     altitude = 100,
-    timeSec = current_time,
     destinationId = BROADCAST_NUM,
     wantAck = False,
     wantResponse = False,
