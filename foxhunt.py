55bb7d7943e9e695c92bd85e69d47e7fade3c06f
tekstrand <tom@teksdesign.com>
2024-12-30 18:23:41 -0600

    Fox hunt improvements

--- foxhunt.py
+++ foxhunt.py
@@ -2,18 +2,32 @@ import meshtastic.serial_interface
 from meshtastic.protobuf import telemetry_pb2, portnums_pb2
 from pubsub import pub
 import time
+import argparse  # Import the argparse library
 
-remote_node = 182032979
+# Setup the argument parser
+parser = argparse.ArgumentParser(description='Send and receive RSSI/SNR data.')
+parser.add_argument('remote_node', type=int, help='Remote node ID to communicate with.')
+args = parser.parse_args()
+
+# Use the remote_node from the command line arguments
+remote_node = args.remote_node
 want_response = True
 channel = 0
-run = True
 
 interface = meshtastic.serial_interface.SerialInterface()
 
 telemetry_data = telemetry_pb2.Telemetry()
 telemetry_data.device_metrics.battery_level = 69
 
+response_received = False
+last_request_time = None
+
 def signalReport():
+    global last_request_time
+    global response_received
+    print("Requesting data...")
+    response_received = False
+    last_request_time = time.time()  # Update last request time
     interface.sendData(
         telemetry_data,
         destinationId=remote_node,
@@ -22,17 +36,25 @@ def signalReport():
     )
 
 def onReceive(packet, interface):
-    global run
-    if packet['from'] == remote_node: 
+    global response_received
+    if packet['from'] == remote_node:
         print(f"\nSNR: {packet['rxSnr']} RSSI: {packet['rxRssi']} \n\n")
-        # print(f"{packet} \n\n")
-        run = False
+        response_received = True
 
 pub.subscribe(onReceive, 'meshtastic.receive')
 
-signalReport()
+signalReport()  # Initial request
 
-while run:
-    time.sleep(1)
-
-interface.close()
+try:
+    while True:
+        time.sleep(0.1)  # Short sleep to allow quick response check
+        if response_received:
+            signalReport()  # Send another request after receiving a response
+        elif time.time() - last_request_time > 10:
+            print("No response received for 10 seconds, retrying...")
+            signalReport()  # Retry if no response received for 10 seconds
+except KeyboardInterrupt:
+    print("Exiting program...")
+finally:
+    interface.close()
+    print("Interface closed. Goodbye!")
