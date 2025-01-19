751b68674c2ee14fb2984f9244f25491e3b27f3f
pdxlocations <benlipsey@gmail.com>
2025-01-18 21:44:03 -0800

    update print-traceroute

--- print-traceroute.py
+++ print-traceroute.py
@@ -1,8 +1,15 @@
-import meshtastic.serial_interface
+
 from pubsub import pub
 import time
-
+import meshtastic.serial_interface, meshtastic.tcp_interface
 interface = meshtastic.serial_interface.SerialInterface()
+# interface = meshtastic.tcp_interface.TCPInterface(hostname="192.168.86.39")
+
+dest = 1623194643
+hopLimit = 5
+channelIndex = 0
+
+interface.sendTraceRoute(dest, hopLimit, channelIndex=channelIndex)
 
 def onReceive(packet, interface):
     if 'decoded' in packet and packet['decoded']['portnum'] == 'TRACEROUTE_APP':
