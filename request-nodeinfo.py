abea55332b910dc92a789846c0a1f01a62048718
pdxlocations <benlipsey@gmail.com>
2025-09-19 14:21:33 -0700

    add request-nodeinfo

---
+++ request-nodeinfo.py
@@ -0,0 +1,24 @@
+from meshtastic.protobuf import mesh_pb2
+import meshtastic.serial_interface
+from pubsub import pub
+import time
+
+interface = meshtastic.serial_interface.SerialInterface()
+user = mesh_pb2.User()
+targetid = 1623194643
+
+def onReceive(packet, interface):
+    if packet['from'] == targetid:
+        print(f"{packet} \n")
+
+pub.subscribe(onReceive, 'meshtastic.receive')
+
+interface.sendData(
+    user,
+    destinationId=targetid,
+    portNum=meshtastic.portnums_pb2.NODEINFO_APP,
+    wantResponse=True,
+)
+
+while True:
+    time.sleep(1)
