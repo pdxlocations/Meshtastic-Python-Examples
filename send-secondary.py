3c56832f3668f8a68cd99139edfe2bdf4c222cdb
pdxlocations <benlipsey@gmail.com>
2026-03-01 16:26:56 -0800

    add send to secondary channel

---
+++ send-secondary.py
@@ -0,0 +1,27 @@
+import meshtastic.serial_interface
+from meshtastic import BROADCAST_ADDR
+from pubsub import pub
+import time
+
+reply_message = "Message Received"
+
+interface = meshtastic.serial_interface.SerialInterface()
+def onReceive(packet, interface=None):
+    try:
+        if 'decoded' in packet and packet['decoded']['portnum'] == 'TEXT_MESSAGE_APP':
+            message_bytes = packet['decoded']['payload']
+            message_string = message_bytes.decode('utf-8')
+            print(f"Received: {message_string}")
+            send_message(reply_message)
+
+    except KeyError as e:
+        print(f"Error processing packet: {e}")
+
+pub.subscribe(onReceive, 'meshtastic.receive')
+
+def send_message(message):
+    interface.sendText(message, destinationId=BROADCAST_ADDR, channelIndex=1)
+    print (f"Sent: {reply_message}")
+
+while True:
+    time.sleep(1)
