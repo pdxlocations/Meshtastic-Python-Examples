199457f25e4e980997c68a321009325f330698ca
pdxlocations <benlipsey@gmail.com>
2026-03-01 16:25:25 -0800

    send our nodeinfo when requesting

--- request-nodeinfo.py
+++ request-nodeinfo.py
@@ -2,21 +2,31 @@ from meshtastic.protobuf import mesh_pb2
 import meshtastic.serial_interface
 from pubsub import pub
 import time
+import base64
 
-targetid = 1623194643
-
+target_id = 576335798
 interface = meshtastic.serial_interface.SerialInterface()
-user = mesh_pb2.User()
+
+# Build User protobuf from local node info
+local_info = interface.getMyNodeInfo()
+local_user = mesh_pb2.User()
+
+local_user.id = local_info["user"]["id"]
+local_user.long_name = local_info["user"]["longName"]
+local_user.short_name = local_info["user"]["shortName"]
+local_user.hw_model = local_info["user"]["hwModel"]
+public_key_b64 = local_info["user"]["publicKey"]
+local_user.public_key = base64.b64decode(public_key_b64)
 
 def onReceive(packet, interface):
-    if packet['from'] == targetid:
-        print(f"{packet} \n")
+    if packet["from"] == target_id:
+        print(f"{packet}\n")
 
-pub.subscribe(onReceive, 'meshtastic.receive')
+pub.subscribe(onReceive, "meshtastic.receive")
 
 interface.sendData(
-    user,
-    destinationId=targetid,
+    local_user,
+    destinationId=target_id,
     portNum=meshtastic.portnums_pb2.NODEINFO_APP,
     wantResponse=True,
 )
